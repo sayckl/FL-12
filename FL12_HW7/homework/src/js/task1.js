a064ed07691ccfea072a50f8eb52fc0020907b95
sayckl <dr.gauss196@gmail.com>
2019-12-29 21:43:57 +0200

    hw7_begin

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,47 @@
 // Your code goes here
+
+let login = prompt('Enter your e-mail');
+
+if ( login === '' ) {
+    alert('Canceled');
+}else if ( login.length < 5 ) {
+    alert("I don't know any emails having name length less than 5 symbols");
+}else if ( login === 'user@gmail.com' || login === 'admin@gmail.com' ) {
+    let password = prompt('Enter your password');
+    if ( password === '' ) {
+        alert('Canceled');
+    }else if ( (login === 'user@gmail.com' && password !== 'UserPass') || (login === 'admin@gmail.com' && password !== 'AdminPass') ) {
+        alert('Wrong password');
+    }else{
+    let changePass = confirm('Do you want to change your password?');
+
+    if ( changePass === true ) {
+    let oldPassword = prompt('Enter your OLD password');
+    if ( password === '' ) {
+        alert('Canceled');
+    }else if ( (login === 'user@gmail.com' && oldPassword.value !== password.value ) || (login === 'admin@gmail.com' && oldPassword.value !== password.value) ) {
+        alert('Wrong password');
+    } else {
+        let newPassword = prompt('Enter your NEW password');
+        if ( newPassword === '' ) {
+            alert('Canceled');
+        }else if ( newPassword.length < 6 ) {
+            alert("It’s too short password. Sorry.");
+        }else{
+            let repeatPassword = prompt('Repeat NEW password');
+            if ( repeatPassword.value !== newPassword.value ) {
+                alert('You wrote the wrong password.');
+            }else{
+                alert('You have successfully changed your password.');
+            }
+        }
+    }
+}else{
+    alert('You have failed the change.');
+}
+}
+}else {
+    alert("I don’t know you");
+}
+
+
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,65 @@
 // Your code goes here
+
+let game = confirm('Do you want to play a game?');
+let repeatGame = true;
+let totalPrize = 0;
+let maxPrize = 100;
+let midlPrize = maxPrize / 2;
+let minPrize = maxPrize / 4;
+let prize = maxPrize;
+let attempt;
+let enterNumber;
+let randomNum; // = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+
+if ( game === true ) {
+    while(repeatGame){
+        for(attempt = 3; attempt > 0; attempt--) {
+            randomNum = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+            enterNumber = prompt(`Choose a roulette pocket number from 0 to 8.` +
+            `\n\rAttempts left: ${attempt}` +
+            `\n\rTotal prize: ${totalPrize}$` +
+            `\n\rPossible prize on current attempt: ${prize}$`, '');
+            if(enterNumber === randomNum){
+				if(attempt === 3){
+					totalPrize += maxPrize;
+                    attempt = 0;
+				} else if(attempt === 2){
+					totalPrize += midlPrize;
+                    attempt = 0;
+				} else if(attempt === 1){
+                    totalPrize += minPrize;
+				}
+				repeatGame = true;
+			} else {
+				repeatGame = false;
+            }
+            if(repeatGame === true){
+                repeatGame = confirm(`Congratulation, you won! Your prize is: ${totalPrize} $. Do you want to continue?`);
+                if(repeatGame){
+                    maxPrize *= 2;
+                    midlPrize = maxPrize / 2;
+                    minPrize = maxPrize / 4;
+                    attempt = 3;
+                    randomNum += 4;
+                } else {
+                    alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+                    break;
+                }
+            } else {
+                alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+                game = confirm('Do you want to play a game?')
+                if(repeatGame){
+                    attempt = 0;
+                    maxPrize = 100;
+                    midlPrize = maxPrize / 2;
+                    minPrize = maxPrize / 4;
+                    totalPrize = 0;
+                } else {
+                    break;
+                }
+            }
+    }
+}
+    }else{
+        alert('You did not become a billionaire, but can.');
+}
