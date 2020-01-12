bcaac708eef7b17f769c027d8c59277c97768505
sayckl <dr.gauss196@gmail.com>
2020-01-12 17:29:44 +0200

    hw9_begin

---
+++ FL12_HW9/homework/index.js
@@ -0,0 +1,14 @@
+// Your code goes here
+
+function convert() {
+    let arg = [];
+    arg.push(convert);
+    arg.forEach(element => {
+        if ( element = NaN ) {
+            return element.splite('');
+        }else{
+            return element.join(' ');
+        }
+    });
+}
+
